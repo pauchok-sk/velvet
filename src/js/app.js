aa4dc094a36ae712c0132139bec69259946c13a1
pauchok-sk <pauchoooooook@gmail.com>
2025-06-12 16:15:05 +0300

    s-models

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import burger from "./files/burger.js";
 import headerMobLocation from "./files/headerMobLocation.js";
 import inputmask from "./files/inputmask.js";
 import showTel from "./files/showTel.js";
+import sliders from "./files/sliders.js";
 import spoller from "./files/spoller.js";
 
 spoller();
@@ -10,4 +11,6 @@ headerMobLocation();
 showTel();
 burger();
 inputmask();
+sliders();
+
 Fancybox.bind("[data-fancybox]", {});
---
+++ src/js/files/sliders.js
@@ -0,0 +1,22 @@
+export default function sliders() {
+  const modelsSlider = document.querySelector(".s-models__slider");
+
+  if (modelsSlider) {
+    const swiper = new Swiper(modelsSlider, {
+      speed: 800,
+      slidesPerView: "auto",
+      spaceBetween: 12,
+      grabCursor: true,
+      scrollbar: {
+        el: ".s-models .slider-scrollbar",
+        draggable: true,
+      },
+      breakpoints: {
+        480: {
+          slidesPerView: "auto",
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+}
