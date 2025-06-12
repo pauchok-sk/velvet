5de6ab37219b9bedb6c7e888bb21bd91e617900a
pauchok-sk <pauchoooooook@gmail.com>
2025-06-12 12:53:13 +0300

    s-atmosphere

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import "../scss/style.scss";
 import burger from "./files/burger.js";
 import headerMobLocation from "./files/headerMobLocation.js";
+import inputmask from "./files/inputmask.js";
 import showTel from "./files/showTel.js";
 import spoller from "./files/spoller.js";
 
@@ -8,3 +9,5 @@ spoller();
 headerMobLocation();
 showTel();
 burger();
+inputmask();
+Fancybox.bind("[data-fancybox]", {});
